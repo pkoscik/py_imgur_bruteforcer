78f33ee44e4f333c6b37d4b583be06e31e19aeb7
crbby <koscikpatryk@gmail.com>
2021-03-24 23:10:53 +0100

    Added: Input sanitization Made help more user friendly

--- bruteforcer.py
+++ bruteforcer.py
@@ -13,6 +13,15 @@ worker_count = 0
 url_found = 0
 
 
+def limits(workers, lenght, extention, type):
+    if workers < 1: return True
+    if lenght not in [5, 7]: return True
+    if extention not in ['png', 'jpg', 'jpeg', 'gif']: return True
+    if type not in [1, 2, 3]: return True
+
+    return False
+
+
 def worker(lenght, extention, type):
     global worker_count
     global url_found
@@ -39,20 +48,25 @@ def worker(lenght, extention, type):
 
 if __name__ == "__main__":
 
-    print('py_imgur_bruteforcer, a simple utility to find and download randomly found imgur images')
-
     argl = len(argv)
 
     # inccorect arguments
     if argl != 1 and argl != 5:
-        print('incorrect arguments, exitting')
-        exit(-1)
+        print(''
+              '\n\n'
+              'py_imgur_bruteforcer, a simple utility to find and download randomly found imgur images\n\n'
+              'bruteforcer.py [workers] [ext] [len] [type]\n'
+              '\t[workers] -> int, no. of workers\n'
+              '\t[ext] -> string,  filename extention - png, jpg, jpeg, gif\n'
+              '\t[len] -> int, lenght of imgur_id - 5 or 7\n'
+              '\t[type] -> int, type of ids to scan - 1: letters 2: digits 3: mixed\n')
+        exit(1)
 
     # if no arguments are passed ask user for input
     if argl == 1:
         worker_ammount = int(input('How many workers to run?\n'))
-        url_ext = input('File extension? (png, jpg, jpeg)\n')
-        url_len = int(input('Url lenght? (5 or 7)\n'))
+        url_ext = input('File extension? (png, jpg, jpeg, gif)\n')
+        url_len = int(input('Url lenght? (5, 7)\n'))
         url_type = int(input('Type of url? ( 1: letters, 2: digits, 3: mixed) \n'))
 
     # correct arguments
@@ -61,6 +75,11 @@ if __name__ == "__main__":
         url_ext = argv[2]
         url_len = int(argv[3])
         url_type = int(argv[4])
+
+    # check input
+    if limits(worker_ammount, url_len, url_ext, url_type):
+        print('Incorrect values for one or more fields, check your inputs and try again')
+        exit(1)
 
     # debug info
     print('Starting', worker_ammount, 'workers')
